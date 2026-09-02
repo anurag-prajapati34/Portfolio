28aabfe02698b867e74b8986e116b0c3686f87f6
Anurag Prajapati <prajapatianurag73240@gmail.com>
2026-09-02 23:45:14 +0530

    feat: google analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="min-h-full flex flex-col">
         {children}
       </body>
+      <GoogleAnalytics gaId="G-6YJX46BD23" />
     </html>
   );
 }
