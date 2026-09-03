29f2747dd812495793815b1d6e820ebd36be5a6b
Anurag Prajapati <prajapatianurag73240@gmail.com>
2026-09-03 12:29:16 +0530

    feat: url preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,33 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Anurag Prajapati",
-  description: "Anurag Prajapati's portfolio website",
+  description:
+    "Software Engineer",
+
+  openGraph: {
+    title: "Anurag Prajapati",
+    description:
+      "Software Engineer",
+    url: "https://anuragdev.com",
+    siteName: "Anurag Prajapati",
+    images: [
+      {
+        url: "https://anuragdev.com/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Anurag Prajapati",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Anurag Prajapati",
+    description:
+      "Software Engineer",
+    images: ["https://anuragdev.com/og-image.png"],
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
